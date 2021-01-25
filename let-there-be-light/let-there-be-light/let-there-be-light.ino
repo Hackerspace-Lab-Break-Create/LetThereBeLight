0bce04bac992e08ddbdb88c2d34615687ff9bf77
Kevin Dias <kevind@backbase.com>
2021-01-25 14:08:09 -0100

    save

--- let-there-be-light/let-there-be-light/let-there-be-light.ino
+++ let-there-be-light/let-there-be-light/let-there-be-light.ino
@@ -1,4 +1,5 @@
-#include <LinkedList.h>
+//#include "LedWord.h"
+//#include "PovAlphabet8x8.h"
 
 const int WORD_LENGTH = 6;
 const int LETTER_HEIGHT = 8;
@@ -9,135 +10,31 @@ const int BUFFER_LENGTH = VIEW_SIZE * 8;
 float roundTripTime = .05; //// Time (Hz) for 1 cycle. 1 complete buffer push
 
 
-class Letter {
-  public:
-    boolean *line0;
-    boolean *line1;
-    boolean *line2;
-    boolean *line3;
-    boolean *line4;
-    boolean *line5;
-    boolean *line6;
-    boolean *line7;
-
-
-    boolean* getLine (int line) {
-      if (line == 0) {
-        return line0;
-      } else if (line == 1) {
-        return line1;
-      } else if (line == 2) {
-        return line2;
-      } else if (line == 3) {
-        return line3;
-      } else if (line == 4) {
-        return line4;
-      } else if (line == 5) {
-        return line5;
-      } else if (line == 6) {
-        return line6;
-      } else if (line == 7) {
-        return line7;
-      }
-
-    }
+byte ALPHABET[3][8]={
+  {// A
+  0b01110000,
+  0b10001000,
+  0b10001000,
+  0b11111000,
+  0b10001000,
+  0b10001000,
+  0b10001000,
+  0b00000000},
+  // Blank
+  {0,0,0,0,0,0,0,0},
+  // Full
+  {1,1,1,1,1,1,1,1}
 };
-
-
-struct LetterA {
-  boolean line0[8] = {0, 1, 1, 1, 0, 0, 0, 0};
-  boolean line1[8] = {1, 0, 0, 0, 1, 0, 0, 0};
-  boolean line2[8] = {1, 0, 0, 0, 1, 0, 0, 0};
-  boolean line3[8] = {1, 1, 1, 1, 1, 0, 0, 0};
-  boolean line4[8] = {1, 0, 0, 0, 1, 0, 0, 0};
-  boolean line5[8] = {1, 0, 0, 0, 1, 0, 0, 0};
-  boolean line6[8] = {1, 0, 0, 0, 1, 0, 0, 0};
-  boolean line7[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-
-} letterA;
-
-
-struct LetterB {
-  boolean line0[8] = {1, 1, 1, 1, 0, 0, 0, 0};
-  boolean line1[8] = {1, 0, 0, 0, 1, 0, 0, 0};
-  boolean line2[8] = {1, 0, 0, 0, 1, 0, 0, 0};
-  boolean line3[8] = {1, 1, 1, 1, 0, 0, 0, 0};
-  boolean line4[8] = {1, 0, 0, 0, 1, 0, 0, 0};
-  boolean line5[8] = {1, 0, 0, 0, 1, 0, 0, 0};
-  boolean line6[8] = {1, 1, 1, 1, 0, 0, 0, 0};
-  boolean line7[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-
-} letterB;
-
-struct LetterC {
-  boolean line0[8] = {0, 1, 1, 1, 0, 0, 0, 0};
-  boolean line1[8] = {1, 0, 0, 0, 1, 0, 0, 0};
-  boolean line2[8] = {1, 0, 0, 0, 0, 0, 0, 0};
-  boolean line3[8] = {1, 0, 0, 0, 0, 0, 0, 0};
-  boolean line4[8] = {1, 0, 0, 0, 0, 0, 0, 0};
-  boolean line5[8] = {1, 0, 0, 0, 1, 0, 0, 0};
-  boolean line6[8] = {0, 1, 1, 1, 0, 0, 0, 0};
-  boolean line7[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-
-} letterC;
-
-
-struct LetterO {
-  boolean line0[8] = {0, 1, 1, 1, 0, 0, 0, 0};
-  boolean line1[8] = {1, 0, 0, 0, 1, 0, 0, 0};
-  boolean line2[8] = {1, 0, 0, 0, 1, 0, 0, 0};
-  boolean line3[8] = {1, 0, 0, 0, 1, 0, 0, 0};
-  boolean line4[8] = {1, 0, 0, 0, 1, 0, 0, 0};
-  boolean line5[8] = {1, 0, 0, 0, 1, 0, 0, 0};
-  boolean line6[8] = {0, 1, 1, 1, 0, 0, 0, 0};
-  boolean line7[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-} letterO;
-
-struct LetterBlank {
-  boolean line0[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-  boolean line1[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-  boolean line2[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-  boolean line3[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-  boolean line4[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-  boolean line5[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-  boolean line6[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-  boolean line7[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-} letterBlank;
-
-
-struct LetterFull {
-  boolean line0[8] = {1, 1, 1, 1, 1, 1, 1, 1};
-  boolean line1[8] = {1, 1, 1, 1, 1, 1, 1, 1};
-  boolean line2[8] = {1, 1, 1, 1, 1, 1, 1, 1};
-  boolean line3[8] = {1, 1, 1, 1, 1, 1, 1, 1};
-  boolean line4[8] = {1, 1, 1, 1, 1, 1, 1, 1};
-  boolean line5[8] = {1, 1, 1, 1, 1, 1, 1, 1};
-  boolean line6[8] = {1, 1, 1, 1, 1, 1, 1, 1};
-  boolean line7[8] = {1, 1, 1, 1, 1, 1, 1, 1};
-} letterFull;
-
 
 class LedWord {
 
   public:
-    LinkedList<Letter> letters; 
+    byte *letters[WORD_LENGTH]; 
     
     LedWord() {
       
-      Letter newLetter;
-
-      newLetter.line0 = letterBlank.line0;
-      newLetter.line1 = letterBlank.line1;
-      newLetter.line2 = letterBlank.line2;
-      newLetter.line3 = letterBlank.line3;
-      newLetter.line4 = letterBlank.line4;
-      newLetter.line5 = letterBlank.line5;
-      newLetter.line6 = letterBlank.line6;
-      newLetter.line7 = letterBlank.line7;
-      
       for (int i = 0; i < WORD_LENGTH; i++) {
-        
-        letters.add(newLetter);
+        setLetter(i, 'A');
       }
     }
 
@@ -147,95 +44,22 @@ class LedWord {
     void setLetter (int letterIndex, char letter);
 
   private:
-    int currentIndex = 0;
+    
+    //bitRead reads a bit starting on the LSB (right-most bit) and we want to start from the left (MSB)
+    int currentIndex = LETTER_WIDTH;
+    
+    // The letters are read as normal from left to right
     int currentLetter = 0;
     boolean ret[LETTER_HEIGHT];
 
 };
 
+
+
 void LedWord::setLetter (int letterIndex, char letter) {
 
-  Letter newLetter;
+    letters[letterIndex] = ALPHABET[(letter-65)];
 
-  if (letter == 'a' || letter == 'A') {
-
-    newLetter.line0 = letterA.line0;
-    newLetter.line1 = letterA.line1;
-    newLetter.line2 = letterA.line2;
-    newLetter.line3 = letterA.line3;
-    newLetter.line4 = letterA.line4;
-    newLetter.line5 = letterA.line5;
-    newLetter.line6 = letterA.line6;
-    newLetter.line7 = letterA.line7;
-
-    letters.set(letterIndex, newLetter);
-
-
-  } else if (letter == 'b' || letter == 'B') {
-
-    newLetter.line0 = letterB.line0;
-    newLetter.line1 = letterB.line1;
-    newLetter.line2 = letterB.line2;
-    newLetter.line3 = letterB.line3;
-    newLetter.line4 = letterB.line4;
-    newLetter.line5 = letterB.line5;
-    newLetter.line6 = letterB.line6;
-    newLetter.line7 = letterB.line7;
-
-    letters.set(letterIndex, newLetter);
-
-  } else if (letter == 'c' || letter == 'C') {
-
-    newLetter.line0 = letterC.line0;
-    newLetter.line1 = letterC.line1;
-    newLetter.line2 = letterC.line2;
-    newLetter.line3 = letterC.line3;
-    newLetter.line4 = letterC.line4;
-    newLetter.line5 = letterC.line5;
-    newLetter.line6 = letterC.line6;
-    newLetter.line7 = letterC.line7;
-
-    letters.set(letterIndex, newLetter);
-
-  } else if (letter == 'o' || letter == 'O') {
-
-    newLetter.line0 = letterO.line0;
-    newLetter.line1 = letterO.line1;
-    newLetter.line2 = letterO.line2;
-    newLetter.line3 = letterO.line3;
-    newLetter.line4 = letterO.line4;
-    newLetter.line5 = letterO.line5;
-    newLetter.line6 = letterO.line6;
-    newLetter.line7 = letterO.line7;
-
-    letters.set(letterIndex, newLetter);
-
-  } else if (letter == '$' || letter == '$') {
-
-    newLetter.line0 = letterFull.line0;
-    newLetter.line1 = letterFull.line1;
-    newLetter.line2 = letterFull.line2;
-    newLetter.line3 = letterFull.line3;
-    newLetter.line4 = letterFull.line4;
-    newLetter.line5 = letterFull.line5;
-    newLetter.line6 = letterFull.line6;
-    newLetter.line7 = letterFull.line7;
-
-    letters.set(letterIndex, newLetter);
-
-  } else {
-
-    newLetter.line0 = letterBlank.line0;
-    newLetter.line1 = letterBlank.line1;
-    newLetter.line2 = letterBlank.line2;
-    newLetter.line3 = letterBlank.line3;
-    newLetter.line4 = letterBlank.line4;
-    newLetter.line5 = letterBlank.line5;
-    newLetter.line6 = letterBlank.line6;
-    newLetter.line7 = letterBlank.line7;
-
-    letters.set(letterIndex, newLetter);
-  }
 }
 
 void LedWord::setWord (char *word, int length) {
@@ -254,21 +78,16 @@ void LedWord::setWord (char *word, int length) {
 
 boolean* LedWord::nextLine () {
 
-  Letter theLetter = letters.get(currentLetter);
+  byte *letter = letters[currentLetter];
 
-  ret[0] = theLetter.line0[currentIndex];
-  ret[1] = theLetter.line1[currentIndex];
-  ret[2] = theLetter.line2[currentIndex];
-  ret[3] = theLetter.line3[currentIndex];
-  ret[4] = theLetter.line4[currentIndex];
-  ret[5] = theLetter.line5[currentIndex];
-  ret[6] = theLetter.line6[currentIndex];
-  ret[7] = theLetter.line7[currentIndex];
+  for (int i=0;i<LETTER_HEIGHT;i++){
+    ret[i] = bitRead(letter[i], currentIndex);
+  }
 
-  currentIndex++;
+  currentIndex--;
 
-  if (currentIndex >= LETTER_HEIGHT) {
-    currentIndex = 0;
+  if (currentIndex <= 0) {
+    currentIndex = LETTER_WIDTH;
 
     currentLetter++;
     if (currentLetter >= WORD_LENGTH) {
@@ -346,7 +165,8 @@ void setup()
   pinMode (6, OUTPUT);
   pinMode (5, OUTPUT);
 
-  ledWord.setWord("A$CAOA", 6);
+  char toDisplay[] = "AAAAAA";
+  ledWord.setWord(toDisplay, 6);
 
 
 }
