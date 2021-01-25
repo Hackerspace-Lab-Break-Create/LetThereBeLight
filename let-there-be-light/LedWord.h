1cd938efa4b8fe471ca8288c91f67a6cccdf2fe6
Kevin Dias <kevind@backbase.com>
2021-01-25 16:14:47 -0100

    comment alphabet

---
+++ let-there-be-light/LedWord.h
@@ -0,0 +1,72 @@
+#include "PovAlphabet8x8.h"
+class LedWord {
+
+  public:
+    int wordLength, letterHeight, letterWidth;
+    
+    byte *letters; 
+    
+    LedWord(byte *lettersBuffer, int wordLength, const int letterHeight, int letterWidth) {
+      letters = lettersBuffer;
+      this->wordLength = wordLength;
+      this->letterHeight = letterHeight;
+      this->letterWidth = letterWidth;
+      this->ret = new boolean [letterHeight];
+      
+      for (int i = 0; i < wordLength; i++) {
+        setLetter(i, 'A');
+      }
+    }
+
+    boolean* nextLine();
+    void setWord(char *word, int length);
+    void setLetter (int letterIndex, char letter);
+
+  private:
+    
+    //bitRead reads a bit starting on the LSB (right-most bit) and we want to start from the left (MSB)
+    int currentIndex = letterWidth;
+    // The letters are read as normal from left to right
+    int currentLetter = 0;
+    boolean *ret;
+};
+
+void LedWord::setLetter (int letterIndex, char letter) {
+
+    letters[letterIndex] = *ALPHABET[(letter-65)];
+
+}
+
+void LedWord::setWord (char *word, int length) {
+  int i;
+
+  for (i = 0; i < length; i++) {
+    setLetter(i, word[i]);
+  }
+
+  //for the future when there is more than 2 letters, blank out the remaining letters in the lettersBuffer
+  for (; i < wordLength; i++) {
+    setLetter(i, ' ');
+  }
+
+}
+
+boolean* LedWord::nextLine () {
+
+  byte letter = letters[currentLetter];
+  for (int i=0;i<letterHeight;i++){
+    ret[i] = bitRead(letter, currentIndex);
+  }
+
+  currentIndex--;
+
+  if (currentIndex <= 0) {
+    currentIndex = letterWidth;
+
+    currentLetter++;
+    if (currentLetter >= wordLength) {
+      currentLetter = 0;
+    }
+  }
+  return ret;
+}
